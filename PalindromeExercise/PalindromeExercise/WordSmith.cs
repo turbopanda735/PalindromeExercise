29c6f1c0828fcbc097d7ace5068c7cc633ddf95a
Joel <franco4prez@gmail.com>
2022-08-30 14:28:57 -0400

    completed exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word.ToLower() == Reverse(word.ToLower()))
+                return true;
+            else
+                return false;
+        }
+        public string Reverse(string word)
+        {
+            char[] array = word.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        } 
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
-using System;
+﻿using System;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.
+        [InlineData("kayak", true)]
+        [InlineData("poop", true)]
+        [InlineData("palindrome", false)]
+        [InlineData("exercise", false)]
+        public void PalindromeTest(string str, bool expected)
         {
+            //arrange
+            var palindrome = new PalindromeExercise.WordSmith();
 
+            //act
+            var actual = palindrome.IsAPalindrome(str);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
